59aae4120fe32b52da355440653002a9c3e057c2
danimtz1303 <martinez.abril1303@gmail.com>
2025-03-03 13:59:23 -0600

    Add files via upload
    Subi todos los archivos

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,6 @@
+//Obtener el boton por si ID que le puse
+const boton=document.getElementById('botoncito');
+//Agregar un evento al boton
+boton.addEventListener('click', function(){
+alert("Hola ya le diste click");
+});
